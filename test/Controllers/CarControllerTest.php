713614eb2fc86f6e01841b9cc257d3eb8228d2d7
rafaellmacedo <rafaelldemacedo@gmail.com>
2021-10-26 00:47:03 -0300

    Create test Car Controller

--- test/Controllers/CarControllerTest.php
+++ test/Controllers/CarControllerTest.php
@@ -1,5 +1,24 @@
 <?php
 
-class CarControllerTest {
+namespace Formulatg\Test\Controllers;
 
+use Formulatg\Controllers\CarController;
+use Formulatg\Entities\Car;
+use PHPStan\Testing\TestCase;
+
+class CarControllerTest extends TestCase {
+
+    public function testController_ShouldCreateCarExpectedSuccess(): void {
+        $car = [
+            2 => 'Piloto A',
+            3 => 10,
+            4 => 'Black',
+            5 => 1
+        ];
+
+        $carController = new CarController();
+        $message = $carController->store($car);
+        echo $message;
+//        $this->assertEquals();
+    }
 }
